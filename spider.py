4c80813654e201b02fef5902e8496c8a09ef0cb5
Muhammad AbdulMoiz <abdulmoizeng@gmail.com>
2019-05-08 19:45:47 +0500

    demo starter implementation

--- spider.py
+++ spider.py
@@ -17,3 +17,35 @@ once one page visit is completed save it in a file and then repeat the page visi
 At last you will have all the required info
 
 """
+
+import requests
+from parsel import Selector
+
+import time
+start = time.time()
+
+all_links  = [] 
+# GET request to recurship site
+response = requests.get('http://example.webscraping.com/')
+selector = Selector(response.text)
+href_links = selector.xpath('//a/@href').getall()
+all_links += href_links
+
+for link in href_links:
+    try:
+        response = requests.get(link)
+        selector = Selector(response.text)
+        href_links = selector.xpath('//a/@href').getall()
+        all_links += href_links
+        if len(all_links) >=1000:
+          break
+    except Exception as exp:
+        print('Error navigating to link : ', link)
+
+
+# Here "all_links" should have 1000 links in it
+
+
+print(all_images)
+end = time.time()
+print("Time taken in seconds : ", (end-start))
